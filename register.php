3d38cf8eb641c1557e30b0c7f23823457b90942c
Michael <65680649+michaelg100@users.noreply.github.com>
2020-05-21 14:15:53 -0400

    Update register.php

--- register.php
+++ register.php
@@ -27,23 +27,14 @@ if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
 }
 
 
-
-
-// We need to check if the account with that username exists.
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
 	$stmt->store_result();
-	// Store the result so we can check if the account exists in the database.
 	if ($stmt->num_rows > 0) {
-		// Username already exists
 		echo 'Username exists, please choose another!';
 	} else {
-		// Insert new account
-		// Username doesnt exists, insert new account
 		if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, activation_code) VALUES (?, ?, ?, ?)')) {
-			// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
 			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 			$uniqid = uniqid();
 			$stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $uniqid);
@@ -56,13 +47,11 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 			mail($_POST['email'], $subject, $message, $headers);
 			echo 'Please check your email to activate your account!';
 		} else {
-			// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
 			echo 'Could not prepare statement!';
 		}
 	}
 	$stmt->close();
 } else {
-	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
 	echo 'Could not prepare statement!';
 }
 $con->close();
